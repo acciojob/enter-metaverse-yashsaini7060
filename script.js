a00a32b0601dffe12f3fab7252096f640f6d9c63
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-19 23:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,25 +17,5 @@
 
 
 function enterMetaverse() {
-           
-            let statusParagraph = document.getElementById("status");
-
-           
-            if (statusParagraph.textContent === "Enter the Metaverse") {
-               
-                statusParagraph.textContent = "Entered Metaverse";
-
-               
-                let h1Tag = document.createElement("h1");
-				h1Tag.id = "status"
-
-                
-                h1Tag.textContent = "Entered Metaverse";
-
-               
-                statusParagraph.parentNode.replaceChild(h1Tag, statusParagraph);
-            } else {
-               
-                alert("Already entered the Metaverse!");
-            }
+            document.getElementById('status').innerHTML= '<h1> Entered Metaverse</h1>'
         }
