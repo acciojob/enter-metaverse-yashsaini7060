1b67f68ab11a39e951b527148dc09e348b5348d2
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-19 22:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function enterMetaverse(){
+	const element = document.getElementById("status");
+	element.remove();
+	const button = document.getElementById("enterBtn");
+	const body = document.body;
+	const newNode = document.createElement("h1");
+	newNode.id="status";
+	newNode.textContent = "Entered Metaverse";
+	body.insertBefore(newNode, button)
+	// alert("Entered")
+	
+}
